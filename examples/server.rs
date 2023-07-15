891dcb9ae065545bb7c0bcd322336181082154f8
CaptainOachkatzl <dave.hofer@hotmail.com>
2023-07-15 22:04:58 +0200

    "fixed" examples
    only fixed the build errors, might work, might not

--- examples/server.rs
+++ examples/server.rs
@@ -1,4 +1,4 @@
-use bevy::app::ScheduleRunnerSettings;
+use bevy::app::ScheduleRunnerPlugin;
 use bevy::prelude::*;
 use bevy_spicy_networking::{ConnectionId, NetworkData, NetworkServer, ServerNetworkEvent};
 use std::net::SocketAddr;
@@ -8,23 +8,25 @@ mod shared;
 
 fn main() {
     let mut app = App::new();
-    app.insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
-        1.0 / 60.0,
-    )));
+    app.add_plugins(ScheduleRunnerPlugin {
+        run_mode: bevy::app::RunMode::Loop {
+            wait: Some(Duration::from_secs_f64(1. / 60.)),
+        },
+    });
     app.add_plugins(MinimalPlugins);
-    app.add_plugin(bevy::log::LogPlugin::default());
+    app.add_plugins(bevy::log::LogPlugin::default());
 
     // Before we can register the potential message types, we
     // need to add the plugin
-    app.add_plugin(bevy_spicy_networking::ServerPlugin);
+    app.add_plugins(bevy_spicy_networking::ServerPlugin);
 
     // A good way to ensure that you are not forgetting to register
     // any messages is to register them where they are defined!
     shared::server_register_network_messages(&mut app);
 
-    app.add_startup_system(setup_networking);
-    app.add_system(handle_connection_events);
-    app.add_system(handle_messages);
+    app.add_systems(Startup, setup_networking);
+    app.add_systems(Update, handle_connection_events);
+    app.add_systems(Update, handle_messages);
 
     app.run();
 }
@@ -52,14 +54,10 @@ fn setup_networking(mut net: ResMut<NetworkServer>) {
 #[derive(Component)]
 struct Player(ConnectionId);
 
-fn handle_connection_events(
-    mut commands: Commands,
-    net: Res<NetworkServer>,
-    mut network_events: EventReader<ServerNetworkEvent>,
-) {
+fn handle_connection_events(mut commands: Commands, net: Res<NetworkServer>, mut network_events: EventReader<ServerNetworkEvent>) {
     for event in network_events.iter() {
         if let ServerNetworkEvent::Connected(conn_id) = event {
-            commands.spawn_bundle((Player(*conn_id),));
+            commands.spawn(Player(*conn_id));
 
             // Broadcasting sends the message to all connected players! (Including the just connected one in this case)
             net.broadcast(shared::NewChatMessage {
@@ -72,10 +70,7 @@ fn handle_connection_events(
 }
 
 // Receiving a new message is as simple as listening for events of `NetworkData<T>`
-fn handle_messages(
-    mut new_messages: EventReader<NetworkData<shared::UserChatMessage>>,
-    net: Res<NetworkServer>,
-) {
+fn handle_messages(mut new_messages: EventReader<NetworkData<shared::UserChatMessage>>, net: Res<NetworkServer>) {
     for message in new_messages.iter() {
         let user = message.source();
 
